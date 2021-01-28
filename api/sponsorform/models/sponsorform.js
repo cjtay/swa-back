5a5f083f643a7765f6a0f8c02b8a7063175d8652
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-01-28 09:40:43 +0800

    updated config and email notifications

--- api/sponsorform/models/sponsorform.js
+++ api/sponsorform/models/sponsorform.js
@@ -13,16 +13,15 @@ module.exports = {
       try {
         const send = await strapi.plugins["email"].services.email.send({
           to: "cjtay888@gmail.com",
-          subject: "SWA Website Contact Form Notification",
+          subject: "SWA Website Sponsorship Enquiry Notification",
           text: `Name: ${result.name}, Email: ${result.email}, Message: ${result.message}`,
           html: `
-          <p>Name: ${result.name}</p> 
-          <p>Company Name: ${result.company}</p> 
-          <p>Email: ${result.email}</p>
-          <p>Phone: ${result.phone}</p>
-          <p>Address: ${result.address}</p>
-          <p>Message: ${result.message}</p>
-          `,
+                      <p>Name: ${result.name}</p> 
+                      <p>Company Name: ${result.company}</p> 
+                      <p>Email: ${result.email}</p>
+                      <p>Phone: ${result.phone}</p>
+                      <p>Message: ${result.message}</p>
+                      `,
         });
         console.log("sent to sendgrid");
       } catch (err) {
--- config/plugins.js
+++ config/plugins.js
@@ -2,16 +2,16 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   upload: {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: process.env.CLOUDINARY_NAME,
+//       api_key: process.env.CLOUDINARY_API_KEY,
+//       api_secret: process.env.CLOUDINARY_SECRET,
+//     },
+//   },
+// });
 
 module.exports = ({ env }) => ({
   // ...
@@ -26,4 +26,12 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+    },
+  },
 });
